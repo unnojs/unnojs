0c43f0e63fb5593c5bb2e0acac465bd7e031b0c5
Keuller Magalhaes@gmail.com <keuller.magalhaes@gmail.com>
2014-12-29 00:43:35 -0200

    Todomvc implementation

---
+++ examples/todomvc/js/store.js
@@ -0,0 +1,95 @@
+Unno.store('TodoStore', ['$storage'], function(storage) {
+   'use strict';
+
+    function S4() {
+        return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+    }
+
+    var TodoStore = {
+        url:'',
+
+        init: function() {
+            var todos = JSON.parse(storage.get('todos'));
+            if (!todos) {
+                todos = [];
+                storage.add('todos', JSON.stringify(todos));
+            }
+        },
+
+        newId: function() {
+            return (S4() + S4() + "-" + S4() + "-4" + S4().substr(0,3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
+        },
+
+        updateList: function(collection) {
+            storage.add('todos', JSON.stringify(collection));
+        },
+
+        getTodos: function() {
+            this.setData(JSON.parse(storage.get('todos')));
+            this.notify();
+        },
+
+        complete: function(id) {
+            var todos = JSON.parse(storage.get('todos'));
+            todos.forEach(function(todo) {
+                var completed = todo.active;
+                if (todo.id === id) todo.active = !completed;
+            });
+
+            this.updateList(todos);
+            this.getTodos();
+        },
+
+        add: function(item) {
+            var todos = JSON.parse(storage.get('todos'));
+
+            item.id = this.newId();
+            todos.push(item);
+            this.updateList(todos);
+            this.getTodos();
+        },
+
+        remove: function(id) {
+            var todos = JSON.parse(storage.get('todos')),
+                index = -1;
+
+            todos.filter(function(item, idx) {
+                if (item.id === id) index = idx;
+            });
+
+            if (index == -1) return;
+            todos.splice(index, 1);
+            this.updateList(todos);
+            this.getTodos();
+        },
+
+        clear: function() {
+            var todos = JSON.parse(storage.get('todos')),
+                filtered = [], index = -1;
+
+            filtered = todos.filter(function(item, idx) {
+                return (item.active == false);
+            });
+
+            filtered.forEach(function(item) {
+                index = -1;
+                todos.forEach(function(todo, idx) {
+                    if (todo.id === item.id) {
+                        index = idx;
+                        return;
+                    }
+                });
+
+                if (index > -1) {
+                    todos.splice(index, 1);
+                }
+            });
+
+            this.updateList(todos);
+            this.getTodos();
+        }
+
+    };
+
+    return TodoStore;
+});
---
+++ examples/todomvc/js/vendor/unno/README.md
@@ -0,0 +1,43 @@
+UnnoJS
+======
+
+Simple Toolkit to Build Modern Web Applications
+
+## Getting Started
+
+UnnoJS is toolkit based on Flux/React and it is bundled with React-Router. You can check out examples to learn more deeply.
+
+UnnoJS is focused to simplify build reactive Web applications, using React to build UI components, Flux pattern to create a one way data flow and Store to maintain data state between components.
+
+## How to Build
+
+You must need gulp installed globally.
+
+    $ npm install
+    $ gulp
+
+After that into `build` folder you'll be `unno.min.js` file.
+
+## License
+
+The MIT License (MIT)
+
+Copyright (c) 2014 unnojs
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
