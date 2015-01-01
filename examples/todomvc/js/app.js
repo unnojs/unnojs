a1b6d672c892195cb9d9a3d2ed872837adc4077c
Keuller Magalhaes@gmail.com <keuller.magalhaes@gmail.com>
2015-01-01 20:20:27 -0200

    todomvc example removed.

--- examples/todomvc/js/app.js
+++
@@ -1,7 +0,0 @@
-Unno.module('app', ['$react'], function(React) {
-   'use strict';
-
-   var TodoApp = Unno.component('TodoApp');
-
-   React.render(TodoApp({}), document.getElementById('app'));
-});
